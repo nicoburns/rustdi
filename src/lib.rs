b05b8fa01bd37849ff981835e69104bcfa66db72
Nico Burns <nico@nicoburns.com>
2018-10-08 00:10:52 +0100

    Implement injectable with actual borrowing

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,13 @@ use crate::quote::{quote, quote_spanned};
 mod ioc;
 use self::ioc::ServiceContainer;
 
-use syn::{Item, ItemFn, FnArg, ArgCaptured, Type, TypePath, Path, Ident};
+use syn::{Item, ItemFn, FnArg, ArgCaptured, Type, TypePath, TypeReference, Path, Ident};
+
+enum ResolveType {
+    ImmutableBorrow,
+    MutableBorrow,
+}
+use self::ResolveType::*;
 
 #[proc_macro_attribute]
 pub fn inject(_attr: TokenStream, input: TokenStream) -> TokenStream {
@@ -17,68 +23,81 @@ pub fn inject(_attr: TokenStream, input: TokenStream) -> TokenStream {
     println!("input: \"{}\"", input.clone().to_string());
     let func : ItemFn = syn::parse(input.clone()).expect("The inject macro is only supported on functions");
 
-    let arg_types = func.clone().decl.inputs.into_iter()
+    let arg_types_and_mutabilities = func.clone().decl.inputs.into_iter()
         .map(|arg| {
-            if let FnArg::Captured(ArgCaptured { ty: Type::Path(TypePath { qself: None, path }), .. }) = arg {
-                let arg_type = path.clone().segments.into_iter().map(|seg| seg.ident.to_string()).collect::<Vec<_>>().join("::");
-                println!("type: {}", arg_type);
-                path
-            } else {
-                panic!("The inject macro only supports simple type arguments");
-            }
+            if let FnArg::Captured(ArgCaptured{ ty: arg_type, .. }) = arg {
+                if let Type::Reference(TypeReference{ mutability, elem, .. }) = arg_type.clone() {
+                    if let Type::Path(TypePath{ qself: None, path: arg_path }) = *elem {
+
+                        // Print debug info
+                        let arg_mutability_str = match &mutability { Some(_) => "&mut ", None => "&"};
+                        let arg_path_str = arg_path.clone().segments.into_iter().map(|seg| seg.ident.to_string()).collect::<Vec<_>>().join("::");
+                        println!("type: {}{}", arg_mutability_str, arg_path_str);
+
+                        let arg_mutability = match &mutability { Some(_) => MutableBorrow, None => ImmutableBorrow };
+                        (arg_type, arg_path, arg_mutability)
+
+                    } else { panic!("The inject macro only supports simple type arguments");}
+                } else { panic!("The inject macro only supports simple type arguments"); }
+            } else { panic!("The inject macro only supports simple type arguments"); }
         });
 
     // Span types
     let ident = func.ident.clone();
-    let container_type = quote_spanned! {Span::call_site() => &crate::ioc::ServiceContainer};
-    let original_func_ident = Ident::new(format!("{}__orig", ident).as_str(), ident.span());;
+    let container_type = quote_spanned!{Span::call_site() => &crate::ioc::ServiceContainer};
+    let original_func_ident = Ident::new(format!("{}_orig", ident).as_str(), ident.span());;
     let mut original_func = func.clone();
     original_func.ident = original_func_ident.clone();
 
-    let args = quote_spanned!{Span::call_site() => #((*resolver.resolve_read::<#arg_types>().unwrap()).clone()),*};
+    let args = arg_types_and_mutabilities.map(|(arg_type, arg_path, arg_mutability)| {
+        match arg_mutability {
+            ImmutableBorrow => quote_spanned!{Span::call_site() => &*resolver.resolve_read::<#arg_path>().unwrap()},
+            MutableBorrow   => quote_spanned!{Span::call_site() => &mut*resolver.resolve_write::<#arg_path>().unwrap()},
+        }
+    });
+
 
     return quote!{
         
-        #original_func
-        
         fn #ident(resolver: #container_type) {
-            #original_func_ident(#args);
+            #original_func
+            #original_func_ident(#(#args),*);
         }
 
     }.into();
 }
 
-#[proc_macro_attribute]
-pub fn show_streams(_attr: TokenStream, input: TokenStream) -> TokenStream {
+// #[proc_macro_attribute]
+// pub fn show_streams(_attr: TokenStream, input: TokenStream) -> TokenStream {
     
-    //println!("attr: \"{}\"", attr.to_string());
-    //println!("input: \"{}\"", input.to_string());
-    // println!("{}", input..c.count());
+//     //println!("attr: \"{}\"", attr.to_string());
+//     //println!("input: \"{}\"", input.to_string());
+//     // println!("{}", input..c.count());
 
-    // for item in input.clone() {
-    //     println!("{}", item.to_string());
-    // }
+//     // for item in input.clone() {
+//     //     println!("{}", item.to_string());
+//     // }
 
-    let item : Item = syn::parse(input.clone()).expect("failed to parse input");
+//     let item : Item = syn::parse(input.clone()).expect("failed to parse input");
 
-    let func = match item {
-        Item::Fn(func) => func,
-        _ => panic!("The macro is only supported on functions"),
-    };
+//     let func = match item {
+//         Item::Fn(func) => func,
+//         _ => panic!("The macro is only supported on functions"),
+//     };
 
-    for arg in func.decl.inputs {
+//     for arg in func.decl.inputs {
 
-        //if let FnArg::Captured(ArgCaptured{ty: Type::Verbatim, ..})
-        //println!("{:?}\n", arg);
+//         //if let FnArg::Captured(ArgCaptured{ty: Type::Verbatim, ..})
+//         //println!("{:?}\n", arg);
 
-        //if let Captured(ArgCaptured { pat: Ident(PatIdent { by_ref: None, mutability: None, ident: Ident { ident: "_s3", span: #0 bytes(228..231) }, subpat: None }), .. })
-        if let FnArg::Captured(ArgCaptured { ty: Type::Path(TypePath { qself: None, path: Path { segments, .. } }), .. }) = arg {
-            println!("type: {}", segments.into_iter().map(|seg| seg.ident.to_string()).collect::<Vec<_>>().join("::"));
-            //[PathSegment { ident: ident, .. }]
-        }
+//         //if let Captured(ArgCaptured { pat: Ident(PatIdent { by_ref: None, mutability: None, ident: Ident { ident: "_s3", span: #0 bytes(228..231) }, subpat: None }), .. })
+//         if let FnArg::Captured(ArgCaptured { ty: Type::Path(TypePath { qself: None, path: Path { segments, .. } }), .. }) = arg {
+//             println!("type: {}", segments.into_iter().map(|seg| seg.ident.to_string()).collect::<Vec<_>>().join("::"));
+//             //[PathSegment { ident: ident, .. }]
+//         }
 
-    }
+//     }
 
-    input
-}
+//     input
+// }
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use std::sync::Arc;
 use std::sync::RwLock;
-use futures::Future;
-use rustdi_macro::{inject, show_streams};
+use rustdi_macro::inject;
 
 pub mod ioc;
 use crate::ioc::Service;
 
-
 // Dummy types for testing DI with
-struct Request;
-struct Response;
-struct Connection;
-
 #[derive(Clone, Debug)]
 struct AppConfig;
 impl Service for AppConfig {}
@@ -23,34 +17,41 @@ pub mod s3 {
     impl Service for S3Client {}
 }
 
-
 fn main() {
-
-    // #[inject]
-    // fn show(_req: Request, _db: Connection, _s3: self::s3::S3Client) -> impl Future<Item=Response, Error=()> {
-    //     return futures::future::ok(Response {});
-    // }
 
     let mut container = ioc::ServiceContainer::new();
     container.bind_singleton(Arc::new(AppConfig));
-    container.bind_singleton(Arc::new(RwLock::new(s3::S3Client("hello".into()))));
+    container.bind_singleton(Arc::new(RwLock::new(s3::S3Client("world".into()))));
 
     let container = container.freeze();
     let thread_container = container.clone();
-    std::thread::spawn(move || {
-        let mut client = thread_container.resolve_write::<s3::S3Client>().unwrap();
-        client.0 = "world".into();
-    }).join().unwrap();
 
     println!("Testing container manually...");
-    let client = container.resolve_read::<s3::S3Client>().unwrap();
-    println!("{}", client.0);
+    std::thread::spawn(move || {
+        let mut client = thread_container.resolve_write::<s3::S3Client>().unwrap();
+        client.0 = "frogs".into();
+    }).join().unwrap();
+    {
+        let client = container.resolve_read::<s3::S3Client>().unwrap();
+        println!("Hello {}", client.0);
+    }
 
     println!("Testing injectable handler...");
-    handler(&container);
+    write_handler(&container);
+    read_handler(&container);
 }
 
 #[inject]
-fn handler(config: AppConfig, client: s3::S3Client) {
-    println!("{}", client.0);
+fn write_handler(_config: &AppConfig, client: &mut s3::S3Client) {
+    client.0 = "penguins".into();
 }
+
+#[inject]
+fn read_handler(_config: &AppConfig, client: &s3::S3Client) {
+    println!("Hello {}", client.0);
+}
+
+// #[inject]
+// fn show(_req: Request, _db: Connection, _s3: self::s3::S3Client) -> impl Future<Item=Response, Error=()> {
+//     return futures::future::ok(Response {});
+// }
