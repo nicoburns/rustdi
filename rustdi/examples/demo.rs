ab0834d8b4b070066a8401a59295f7b32e0369ab
Nico Burns <nico@nicoburns.com>
2018-10-08 01:50:31 +0100

    Add panic causing handler

--- rustdi/examples/demo.rs
+++ rustdi/examples/demo.rs
@@ -28,6 +28,11 @@ fn read_handler(_config: &AppConfig, client: &s3::S3Client) {
     println!("Hello {}", client.0);
 }
 
+#[inject]
+fn invalid_handler(_config: &mut AppConfig) {
+    println!("Hello world");
+}
+
 // #[inject]
 // fn show(_req: Request, _db: Connection, _s3: self::s3::S3Client) -> impl Future<Item=Response, Error=()> {
 //     return futures::future::ok(Response {});
@@ -71,4 +76,9 @@ fn main() {
         let container = container.clone();
         move || { read_handler(&container); }
     }).join().unwrap();
+
+    // Testing invalid handler. We cannot get a mutable reference to
+    // an Arc singleton so we just panic in this case.
+    println!("Testing invalid handler (expect panic)...");
+    invalid_handler(&container);
 }
